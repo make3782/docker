d5ea0ebcb45359eec420ccff47fac1e12a666287
make3782 <181788481@qq.com>
2017-12-06 14:58:24 +0800

    lnmp

---
+++ lnmp/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+icat >>/etc/supervisord.conf<<-EOF
+[supervisord]
+nodaemon=true
+[program:DOClever]
+stdout_logfile_maxbytes=100MB
+stdout_logfile=/tmp/debug.log
+stderr_logfile=/tmp/error.log
+command=lnmp start
+EOF
+
+# run supervisord
+/usr/bin/supervisord -c /etc/supervisord.conf
